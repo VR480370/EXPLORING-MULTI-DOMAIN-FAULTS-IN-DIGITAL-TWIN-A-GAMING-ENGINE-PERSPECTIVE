fc3b12022caa357fe1959866056c2043258a4aa9
Muhammad Ihtisham Amin <108006537+VR480370@users.noreply.github.com>
2024-07-29 21:22:39 +0200

    Add files via upload

---
+++ CRot.cpp
@@ -0,0 +1,146 @@
+﻿
+#include "CRot.h"
+#include "Kismet/GameplayStatics.h" // Include this for accessing game time
+#include "Misc/FileHelper.h" // Include this for file operations
+#include "Misc/Paths.h" // Include this for file paths
+
+// Sets default values for this actor's properties
+ACRot::ACRot()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    // Create and attach scene component
+    SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+    RootComponent = SceneComponent;
+
+    // Create and attach mesh component
+    ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMesh"));
+    ItemMesh->SetupAttachment(RootComponent);
+
+    // Default rotation rate 
+    RotationRate = FRotator(0.0f, 0.0f, 180.0f);
+
+    // Default motor parameters 
+    Resistance = 0.4f;
+    Inductance = 0.05f;
+    Kt = 1.0f;
+    Kv = 1.25f;
+    J = 0.5f;
+    Friction = 0.001f;
+
+    // Initialize motor state variables
+    Current = 0.0f;
+    AngularVelocity = 0.0f;
+
+    // Initial applied voltage and load torque
+    AppliedVoltage = 0.0f;
+    LoadTorque = 0.0f;
+
+    // Simulation time
+    RealTimeSeconds = 0.0f;
+
+    FilePath = FPaths::ProjectSavedDir() + TEXT("test.txt");
+    FFileHelper::SaveStringToFile(TEXT("RealTimeSeconds \t DeltaTime \t Input Voltage \t Current \t Resistance \t Angular Velocity\n"), *FilePath);
+}
+
+// Called when the game starts or when spawned
+void ACRot::BeginPlay()
+{
+    Super::BeginPlay();
+    ACRot::StartDelayUpdate();
+}
+
+void ACRot::updateAfterDelay()
+{
+    if (this->valuesIndex >= 19)
+    {
+        this->valuesIndex = 0;
+    }
+
+    this->Resistance = this->ResistanceValues[this->valuesIndex];
+
+    this->valuesIndex++;
+
+}
+
+void  ACRot::StartDelayUpdate()
+{
+    GetWorld()->GetTimerManager().SetTimer(this->TimerHandle, this, &ACRot::updateAfterDelay, 1.0f, true);
+}
+
+// Called every frame
+void ACRot::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    // Calculating simulation time
+    RealTimeSeconds += DeltaTime;
+
+    // Desired step
+    float step = 0.05;
+
+    if (DeltaTime < step) {
+
+        EulerDerivative(DeltaTime);
+
+        // Update actor's rotation based on angular velocity
+        FRotator DeltaRotation = RotationRate * DeltaTime * AngularVelocity;
+        AddActorLocalRotation(DeltaRotation);
+
+        // Save data to a text file
+        FString TextData = FString::Printf(TEXT("%.3f \t %.3f \t %.3f \t %.3f \t %.3f \t %.3f\n"), RealTimeSeconds, DeltaTime, AppliedVoltage, Current, Resistance, AngularVelocity);
+        FFileHelper::SaveStringToFile(TextData, *FilePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), FILEWRITE_Append);
+    }
+    else {
+        int iters = int(DeltaTime / step);
+        //int carry = int(DeltaTime % 0.05);
+        for (int c = 0; c < iters; c++) {
+
+            EulerDerivative(step);
+
+            // Update actor's rotation based on angular velocity
+            FRotator DeltaRotation = RotationRate * DeltaTime * AngularVelocity;
+            AddActorLocalRotation(DeltaRotation);
+
+            // Calculating simulation time
+            RealTimeSeconds += step;
+
+            // Save data to a text file
+            FString TextData = FString::Printf(TEXT("%.3f \t %.3f \t %.3f \t %.3f \t %.3f \t %.3f\n"), RealTimeSeconds, (step * c), AppliedVoltage, Current, Resistance, AngularVelocity);
+            FFileHelper::SaveStringToFile(TextData, *FilePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), FILEWRITE_Append);
+        }
+    }
+
+    UE_LOG(LogClass, Log, TEXT("DeltaTime: %f \t RealTimeSeconds: %f \t Current: %f \t Resistance: %f \t Speed: %f"), DeltaTime, RealTimeSeconds, Current, Resistance, AngularVelocity);
+
+    // Begin the input feed
+    if (RealTimeSeconds > 1.0) {
+        AppliedVoltage = 200.0;
+    }
+
+    // Stop the input feed
+    if (RealTimeSeconds > 10.0) {
+        AppliedVoltage = 0.0;
+    }
+}
+
+void ACRot::EulerDerivative(float CustomDT) {
+    // Calculate derivatives
+    float dCurrent_dt = (AppliedVoltage - Kv * AngularVelocity - Resistance * Current) / Inductance;
+    float dAngularVelocity_dt = (Kt * Current - Friction * AngularVelocity - LoadTorque) / J;
+
+    // Euler's method to update state variables
+    Current += dCurrent_dt * CustomDT;
+    AngularVelocity += dAngularVelocity_dt * CustomDT;
+}
+
+void ACRot::SetVoltage(float Voltage)
+{
+    AppliedVoltage = Voltage;
+}
+
+void ACRot::SetLoadTorque(float Torque)
+{
+    LoadTorque = Torque;
+}
+
---
+++ CRot.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Misc/Paths.h"
+#include "Misc/FileHelper.h"
+#include "HAL/PlatformFilemanager.h"
+#include "Kismet/GameplayStatics.h"
+#include "CRot.generated.h"
+
+UCLASS()
+class IMPORTTRY_API ACRot : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ACRot();
+
+    virtual void Tick(float DeltaTime) override;
+
+    // Set applied voltage to the motor
+    void SetVoltage(float Voltage);
+
+    // Set load torque on the motor
+    void SetLoadTorque(float Torque);
+
+    void EulerDerivative(float CustomDT);
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    // Motor parameters
+    float Resistance; // Resistance of the motor
+    float Inductance; // Inductance of the motor
+    float Kt;         // Torque constant
+    float Kv;         // Voltage constant
+    float J;          // Moment of inertia
+    float Friction;          // Viscous friction coefficient
+
+    // Motor state variables
+    float Current;    // Current flowing through the motor
+    float AngularVelocity; // Angular velocity of the motor shaft
+
+    // Applied voltage and load torque
+    float AppliedVoltage;
+    float LoadTorque;
+
+    float RealTimeSeconds;
+
+    // Mesh component representing the motor
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DCMOTOR", meta = (AllowPrivateAccess = "true"))
+    USceneComponent* SceneComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DCMOTOR", meta = (AllowPrivateAccess = "true"))
+    UStaticMeshComponent* ItemMesh;
+
+    // Rotation rate and axis
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "DCMOTOR", meta = (AllowPrivateAccess = "true"))
+    FRotator RotationRate;
+
+    // Output file path
+    FString FilePath;
+
+    void updateAfterDelay();
+    void  StartDelayUpdate();
+
+    FTimerHandle TimerHandle;
+    double ResistanceValues[19] = { 3.8, 4.2, 4.8, 4.9, 5.2, 5.4, 5.8, 6.2, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4 };
+    int valuesIndex = 0;
+};
+
+
+
+
+
+
